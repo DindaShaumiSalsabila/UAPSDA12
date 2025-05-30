142c90f25ea5c296aa4daa0669d5d00641b9b8d0
DindaShaumiSalsabila <dindashaumi67@gmail.com>
2025-05-30 13:03:28 +0700

    Create tkinter.py

---
+++ tkinter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk, font
+import time
+
+class WelcomeApp:
+    def _init_(self, root):
+        self.root = root
+        self.root.title("Welcome & Perkenalan Kelompok")
+        self.root.geometry("800x600") 
+        self.root.configure(bg="#FFFFFF")
