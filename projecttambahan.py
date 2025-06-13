857ed2a08c0edb1e2a2b1039611b20b6ef9021f8
LAKAISHAGBSCODING <faqihlakaishaputra36@gmail.com>
2025-06-13 14:01:48 +0700

    Update projecttambahan.py

--- projecttambahan.py
+++ projecttambahan.py
@@ -1,1 +1,38 @@
+import tkinter as tk
+from tkinter import messagebox, filedialog
+
+class PesertaKarate:
+    def __init__(self, id: str, nama: str):
+        self.id = id
+        self.nama = nama
+        self.skor = 0
+
+    def tambah_skor(self, nilai: int):
+        self.skor += nilai
+
+    def skor_tertinggi(self) -> int:
+        return self.skor
+
+    def rata_skor(self) -> float:
+        return self.skor
+
+class SistemScoringKarate:
+    def __init__(self):
+        self.peserta = {}
+
+    def daftar_peserta(self, id: str, nama: str):
+        id = id.strip().lower()
+        if id not in self.peserta:
+            self.peserta[id] = PesertaKarate(id, nama)
+            return f"✓ Peserta {nama} (ID: {id}) terdaftar"
+        else:
+            return f"× ID {id} sudah ada"
+
+    def tambah_skor(self, id: str, skor: int):
+        id = id.strip().lower()
+        if id in self.peserta:
+            self.peserta[id].tambah_skor(skor)
+            return f"✓ Skor {skor} ditambahkan untuk {self.peserta[id].nama}, total: {self.peserta[id].skor}"
+        else:
+            return f"× ID {id} tidak ditemukan"
 
